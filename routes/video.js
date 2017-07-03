0a11194b3ce83d98a3efbb8f678c3d651a9e16c1
Christo <Christo>
2017-07-03 21:03:11 +0800

    about 1%

--- routes/video.js
+++ routes/video.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('campus/index', { title: 'video' });
+  res.render('video/index', { title: 'video' });
+});
+router.get('/life', function(req, res, next) {
+  res.render('video/life', { title: 'life' });
+});
+router.get('/visit', function(req, res, next) {
+  res.render('video/visit', { title: 'visit' });
 });
 
 module.exports = router;
