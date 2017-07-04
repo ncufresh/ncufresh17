3311dbc97d5753cac1feca509a8dc7fff5903936
cytsai <evan11401@gmail.com>
2017-07-04 15:20:59 +0800

    ckeditor upload to server

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -60,6 +62,30 @@ app.use('/video', video);
 // 個人專區
 app.use('/personal', personal);
 
+
+// ckeditor uploader
+app.post('/uploader', multipartMiddleware, function(req, res) {
+    var fs = require('fs');
+
+    fs.readFile(req.files.upload.path, function (err, data) {
+        var newPath = __dirname + '/public/uploads/' + req.files.upload.name;
+        fs.writeFile(newPath, data, function (err) {
+            if (err) console.log({err: err});
+            else {
+                html = "";
+                html += "<script type='text/javascript'>";
+                html += "    var funcNum = " + req.query.CKEditorFuncNum + ";";
+                html += "    var url     = \"/uploads/" + req.files.upload.name + "\";";
+                html += "    var message = \"Uploaded file successfully\";";
+                html += "";
+                html += "    window.parent.CKEDITOR.tools.callFunction(funcNum, url, message);";
+                html += "</script>";
+
+                res.send(html);
+            }
+        });
+    });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
