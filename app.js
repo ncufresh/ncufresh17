8f0296962ee3228cc6d0a7c4993078f1fb8d0d16
JoNz94 <jonz94@g.ncu.edu.tw>
2017-07-10 11:25:35 +0800

    fix some indent

--- app.js
+++ app.js
@@ -68,26 +68,26 @@ app.use('/about', about);
 
 // ckeditor uploader
 app.post('/uploader', multipartMiddleware, function(req, res) {
-    var fs = require('fs');
+  var fs = require('fs');
 
-    fs.readFile(req.files.upload.path, function (err, data) {
-        var newPath = __dirname + '/public/uploads/' + req.files.upload.name;
-        fs.writeFile(newPath, data, function (err) {
-            if (err) console.log({err: err});
-            else {
-                html = "";
-                html += "<script type='text/javascript'>";
-                html += "    var funcNum = " + req.query.CKEditorFuncNum + ";";
-                html += "    var url     = \"/uploads/" + req.files.upload.name + "\";";
-                html += "    var message = \"Uploaded file successfully\";";
-                html += "";
-                html += "    window.parent.CKEDITOR.tools.callFunction(funcNum, url, message);";
-                html += "</script>";
+  fs.readFile(req.files.upload.path, function (err, data) {
+    var newPath = __dirname + '/public/uploads/' + req.files.upload.name;
+    fs.writeFile(newPath, data, function (err) {
+      if (err) console.log({err: err});
+      else {
+        html = "";
+        html += "<script type='text/javascript'>";
+        html += "    var funcNum = " + req.query.CKEditorFuncNum + ";";
+        html += "    var url     = \"/uploads/" + req.files.upload.name + "\";";
+        html += "    var message = \"Uploaded file successfully\";";
+        html += "";
+        html += "    window.parent.CKEDITOR.tools.callFunction(funcNum, url, message);";
+        html += "</script>";
 
-                res.send(html);
-            }
-        });
+        res.send(html);
+      }
     });
+  });
 });
 
 // catch 404 and forward to error handler
