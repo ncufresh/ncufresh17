074c9ac4be74a6972793f480218fca32056724f9
Richie Cheng <richie19951229@gmail.com>
2017-07-15 02:12:04 +0800

    qaModal

--- routes/about.js
+++ routes/about.js
@@ -3,22 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('about/index', { title: 'about' });
+  res.render('about/index', { title: 'about', user: req.user });
 });
 router.get('/markting', function(req, res, next) {
-  res.render('about/markting', { title: 'about' });
+  res.render('about/markting', { title: 'about', user: req.user });
 });
 router.get('/programming', function(req, res, next) {
-  res.render('about/programming', { title: 'about' });
+  res.render('about/programming', { title: 'about', user: req.user });
 });
 router.get('/ceo', function(req, res, next) {
-  res.render('about/ceo', { title: 'about' });
+  res.render('about/ceo', { title: 'about', user: req.user });
 });
 router.get('/art', function(req, res, next) {
-  res.render('about/art', { title: 'about' });
+  res.render('about/art', { title: 'about', user: req.user });
 });
 router.get('/video', function(req, res, next) {
-  res.render('about/video', { title: 'about' });
+  res.render('about/video', { title: 'about', user: req.user });
 });
 
 module.exports = router;
