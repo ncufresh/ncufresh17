498e35c2694cb8916972d4f43c8a0f0db1534bed
cytsai <evan11401@gmail.com>
2017-07-18 20:41:44 +0800

    portal

--- routes/index.js
+++ routes/index.js
@@ -114,7 +114,7 @@ module.exports = function(passport) {
         console.log('body:'+body);
         personalObj = JSON.parse(body);
         // find user using id(學號)
-        User.find({email: personalObj.id+'@cc.ncu.edu.tw'}).exec(function(err,obj) {
+        User.find({'local.email': personalObj.id+'@cc.ncu.edu.tw'},function(err,obj) {
           console.log(obj);
           // if(obj){
           //   console.log('obj existing');
