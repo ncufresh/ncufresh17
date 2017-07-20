3d53497421ea4e1826da4170f6bead6d11bf2588
Tommk <tommy93211@gmail.com>
2017-07-20 20:43:09 +0800

    back-end 20%

--- routes/life.js
+++ routes/life.js
@@ -1,33 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var url = require('url');
+var fs = require('fs');
+var life = require('../models/life');
 var shortId = require('shortid');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('life/index', { title: 'life', user: req.user });
 });
-router.get('/food', function(req, res, next) {
-  res.render('life/food', { title: 'food', user: req.user });
-});
-router.get('/dorm', function(req, res, next) {
-  res.render('life/dorm', { title: 'dorm', user: req.user });
-});
-router.get('/traffic', function(req, res, next) {
-  res.render('life/traffic', { title: 'traffic', user: req.user });
-});
-router.get('/study', function(req, res, next) {
-  res.render('life/study', { title: 'study', user: req.user });
-});
-router.get('/entertainment', function(req, res, next) {
-  res.render('life/entertainment', { title: 'entertainment', user: req.user });
-});
-router.get('/food/:id',function(req,res,next){
-  department.find({_id:req.params.id},function(err,data){
+router.get('/life/:id',function(req,res,next){
+  life.find({_id:req.params.id},function(err,data){
     res.send(data[0]);
   });
 });
+router.get('/food', function(req, res, next) {
+	life.find({}).exec(function(err,life){
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+  res.render('life/food', {
+  	title: 'life',
+  	user: req.user,
+  	firstClick: req.query.dep,
+    life: life,
+   });
+});
+});
 router.post('/add_life', function(req, res, next) {
-	food({
+	life({
 		type:req.body.type,
 		name:req.body.name,
 		introduction:req.body.introduction,
