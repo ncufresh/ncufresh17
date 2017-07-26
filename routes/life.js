be7ddb4c0151373f5becdfe9625c6e34ec2649c2
cytsai <evan11401@gmail.com>
2017-07-26 11:33:25 +0800

    fixed life bug

--- routes/life.js
+++ routes/life.js
@@ -27,14 +27,33 @@ router.get('/food', function(req, res, next) {
 });
 });
 router.post('/add_life', function(req, res, next) {
-	life({
+  var form = new formidable.IncomingForm();
+	form.parse(req, function (err, fields, files) {
+		if (err) {
+			console.log("上傳err");
+		}
+    var uploadedFile = files.uploadImg;
+		var tmpPath = uploadedFile.path;
+		var fileName = shortId.generate() + uploadedFile.name.substr(uploadedFile.name.lastIndexOf('.'));
+		var targetPath = './public/life/food' + fileName;
+		console.log(tmpPath);
+		console.log(targetPath);
+    var readStream = fs.createReadStream(tmpPath)
+		var writeStream = fs.createWriteStream(targetPath);
+    var a = new life({
 		type:req.body.type,
 		name:req.body.name,
 		introduction:req.body.introduction,
-	}).save(function(err, doc){
-		if(err)res.json(err);
-		else   res.redirect('/life/food');
-	});
+    img_path: '/life/food' + fileName,
+    fileName: fileName
+    });
+    a.save();
+    readStream.on("end", function () {
+    	fs.unlink(tmpPath, function () {
+    		res.send(a);
+    	});
+    }).pipe(writeStream);
+    console.log(fields.imgid);
+  });
 });
-
 module.exports = router;
