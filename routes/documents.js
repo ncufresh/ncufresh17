156cac927f07316a0b24f1e708c18eccaa098331
0522andrew <andrew4408@gmail.com>
2017-07-26 22:41:25 +0800

    新增時可一起上傳圖片

--- routes/documents.js
+++ routes/documents.js
@@ -54,21 +54,64 @@ router.get('/delete/:id',function(req,res,next){
 });
 
 router.post('/add',function(req,res,next){
-  var temp;
-  document.find({type:req.body.type}).exec(function (err, results) {
-    if(err) throw err;
-    temp = results.length;
-    temp++;
-    console.log(temp);
-    var b = new document({
-      name:req.body.name,
-      type:req.body.type,
-      content:req.body.content,
-      img_path: "",
-      order: temp
-    }).save();
+  
+
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files){
+      if(err){
+          console.log("上傳err");
+      }
+      // console.log('received fields: ');
+      // console.log(fields);
+      // console.log('received files: ');
+      // console.log(files);
+
+      var uploadedFile = files.uploadingImg;
+      var tmpPath = uploadedFile.path;
+      var tempId = shortId.generate();
+      var fileName = tempId + uploadedFile.name.substr(uploadedFile.name.lastIndexOf('.'));
+      var targetPath = './public/documents/' + fileName;
+      console.log(tmpPath);
+      console.log(targetPath);
+      // 跨分區會error
+      // fs.rename(tmpPath, targetPath, function(err) {
+      //   if (err){
+      //     console.log(err);
+      //   }
+      //   else{ 
+      //     fs.unlink(tmpPath, function() {
+      //       console.log('File Uploaded to ' + targetPath + ' - ' + uploadedFile.size + ' bytes');
+      //     });
+      //   }
+      // });
+
+      var readStream = fs.createReadStream(tmpPath)
+      var writeStream = fs.createWriteStream(targetPath);
+      readStream.on("end",function(){
+
+        document.find({type:fields.type}).exec(function (err, results) {
+          if(err) throw err;
+          var temp = results.length;
+          temp++;
+          console.log(temp);
+          var b = new document({
+            _id: tempId,
+            name:fields.name,
+            type:fields.type,
+            content:fields.content,
+            img_path:'/documents/' + fileName,
+            order: temp
+          }).save();
+        });
+        res.redirect('/documents');
+      })
+      .pipe(writeStream,function(){
+        fs.unlink(tmpPath, function(err) {
+          if(err)console.log("刪除暫存err")
+          console.log('File Uploaded to ' + targetPath + ' - ' + uploadedFile.size + ' bytes');
+        });
+      });
   });
-  res.redirect('/documents');
 });
 
 router.post('/update',function(req,res,next){
