1771b73c30a4ac3d82583c95020159eb984d1f71
0522andrew <andrew4408@gmail.com>
2017-08-01 00:33:01 +0800

    網站地圖50%

--- routes/aboutweb.js
+++ routes/aboutweb.js
@@ -18,8 +18,17 @@ router.get('/edit_aboutweb', function(req, res, next) {
   });
 });
 
+router.get('/get_data/:id',function(req,res,next){
+  aboutweb.find({_id:req.params.id},function(err,data){
+    if(err) 
+      console.log(err);
+    res.send(data[0]);
+  });
+});
+
 router.post('/edit',function(req,res,next){
 	document.update({_id:req.body.id},{
+      name:req.body.name,
       x:req.body.x,
       y:req.body.y,
       link:req.body.content,
@@ -32,60 +41,15 @@ router.post('/edit',function(req,res,next){
 });
 
 router.post('/add',function(req,res,next){
-  var form = new formidable.IncomingForm();
-  form.parse(req, function(err, fields, files){
-  		console.log(fields.content)
-      if(err){
-          console.log("上傳err");
-      }
-      // console.log('received fields: ');
-      // console.log(fields);
-      // console.log('received files: ');
-      // console.log(files);
-
-      var tempId = shortId.generate();
-      var fileName = "";
-      if(files.uploadingImg.name!="")
-      {
-        var uploadedFile = files.uploadingImg;
-        var tmpPath = uploadedFile.path;
-        fileName = tempId + uploadedFile.name.substr(uploadedFile.name.lastIndexOf('.'));
-        var targetPath = './public/aboutweb/' + fileName;
-        console.log(tmpPath);
-        console.log(targetPath);
-        // 跨分區會error
-        // fs.rename(tmpPath, targetPath, function(err) {
-        //   if (err){
-        //     console.log(err);
-        //   }
-        //   else{ 
-        //     fs.unlink(tmpPath, function() {
-        //       console.log('File Uploaded to ' + targetPath + ' - ' + uploadedFile.size + ' bytes');
-        //     });
-        //   }
-        // });
-
-        var readStream = fs.createReadStream(tmpPath)
-        var writeStream = fs.createWriteStream(targetPath);
-        readStream.on("end",function(){        
-        })
-        .pipe(writeStream,function(){
-          fs.unlink(tmpPath, function(err) {
-            if(err)console.log("刪除暫存err");
-            console.log('File Uploaded to ' + targetPath + ' - ' + uploadedFile.size + ' bytes');
-          });
-        });
-      } 
-      var b = new aboutweb({
-          _id: tempId,
-          link:fields.content,
-          x:0,
-          y:0,
-          size:5,
-          img_path:'/aboutweb/' + fileName,
-        }).save();
-        res.redirect('/aboutweb/edit_aboutweb/');
-    });
+  var b = new aboutweb({
+    name:req.body.name,
+    link:req.body.content,
+    x:0,
+    y:0,
+    size:5,
+    type:req.body.type
+  }).save();
+  res.redirect('/aboutweb/edit_aboutweb/');
 });
 
 module.exports = router;
