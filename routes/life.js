75305742f7b39916e0660c5f0ce71b845e2d1a10
Tommk <tommy93211@gmail.com>
2017-08-01 00:52:24 +0800

    3 more to do

--- routes/life.js
+++ routes/life.js
@@ -60,24 +60,43 @@ router.post('/add_life', function(req, res, next) {
 		var tmpPath1 = uploadedFile1.path;
     var tmpPath2 = uploadedFile2.path;
     var tmpPath3 = uploadedFile3.path;
-		var fileName1 = shortId.generate() + uploadedFile1.name.substr(uploadedFile1.name.lastIndexOf('.'));
-    var fileName2 = shortId.generate() + uploadedFile2.name.substr(uploadedFile2.name.lastIndexOf('.'));
-    var fileName3 = shortId.generate() + uploadedFile3.name.substr(uploadedFile3.name.lastIndexOf('.'));
-		var targetPath1 = './public/life/' + fileName1;
-    var targetPath2 = './public/life/' + fileName2;
-    var targetPath3 = './public/life/' + fileName3;
-		console.log(tmpPath1);
-    console.log(tmpPath2);
-    console.log(tmpPath3);
-		console.log(targetPath1);
-    console.log(targetPath2);
-    console.log(targetPath3);
-    var readStream1 = fs.createReadStream(tmpPath1);
-    var readStream2 = fs.createReadStream(tmpPath2);
-    var readStream3 = fs.createReadStream(tmpPath3);
-		var writeStream1 = fs.createWriteStream(targetPath1);
-    var writeStream2 = fs.createWriteStream(targetPath2);
-    var writeStream3 = fs.createWriteStream(targetPath3);
+    var fileName1 = "";
+    if(uploadedFile1.name!=""){
+      fileName1 = shortId.generate() + uploadedFile1.name.substr(uploadedFile1.name.lastIndexOf('.'));
+      var targetPath1 = './public/life/' + fileName1;
+      console.log(tmpPath1);
+      console.log(targetPath1);
+      var readStream1 = fs.createReadStream(tmpPath1);
+      var writeStream1 = fs.createWriteStream(targetPath1);
+      readStream1.on("end", function () {
+      	fs.unlink(tmpPath1);
+      }).pipe(writeStream1);
+    }
+    var fileName2 = "";
+    if(uploadedFile2.name!=""){
+        fileName2 = shortId.generate() + uploadedFile2.name.substr(uploadedFile2.name.lastIndexOf('.'));
+        var targetPath2 = './public/life/' + fileName2;
+        console.log(tmpPath2);
+        console.log(targetPath2);
+        var readStream2 = fs.createReadStream(tmpPath2);
+        var writeStream2 = fs.createWriteStream(targetPath2);
+        readStream2.on("end", function () {
+        	fs.unlink(tmpPath2);
+        }).pipe(writeStream2);
+    }
+    var fileName3 = "";
+		if( uploadedFile3.name!=""){
+      fileName3 = shortId.generate() + uploadedFile3.name.substr(uploadedFile3.name.lastIndexOf('.'));
+      var targetPath3 = './public/life/' + fileName3;
+      console.log(tmpPath3);
+      console.log(targetPath3);
+      var readStream3 = fs.createReadStream(tmpPath3);
+      var writeStream3 = fs.createWriteStream(targetPath3);
+      readStream3.on("end", function () {
+      	fs.unlink(tmpPath3, function () {
+      	});
+      }).pipe(writeStream3);
+    }
     var a = new life({
 		type:fields.type,
 		name:fields.name,
@@ -89,19 +108,32 @@ router.post('/add_life', function(req, res, next) {
     img_path3: '/life/' + fileName3,
     fileName3: fileName3
     });
-    a.save();
-    readStream1.on("end", function () {
-    	fs.unlink(tmpPath1);
-    }).pipe(writeStream1);
-    readStream2.on("end", function () {
-    	fs.unlink(tmpPath2);
-    }).pipe(writeStream2);
-    readStream3.on("end", function () {
-    	fs.unlink(tmpPath3, function () {
-        res.send(a);
-    	});
-    }).pipe(writeStream3);
-    res.redirect('/life');
+    a.save(function(){
+      res.redirect('/life/food');
+    });
   });
+});
+router.get('/delete_life/:id', function (req, res, next) {
+	console.log(req.params.id);
+	life.findById(req.params.id, function (err, data) {
+      if (data.fileName1!=""){
+        fs.unlink('./public/life/' + data.fileName1, function(err){
+          if (err) console.log(err);
+        });
+      }
+      if (data.fileName2!=""){
+        fs.unlink('./public/life/' + data.fileName2,function(err){
+          if (err) console.log(err);
+        });
+      }
+      if (data.fileName3!=""){
+        fs.unlink('./public/life/' + data.fileName3, function (err) {
+				  if (err) console.log(err);
+        });
+      }
+			life.findById(req.params.id).remove().exec(function(){
+        res.redirect('/life/food');
+      });
+	});
 });
 module.exports = router;
