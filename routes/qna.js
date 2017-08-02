fa38c174a725838e3f21746becb9ff0b0d8d18db
jonz94 <jonz94@g.ncu.edu.tw>
2017-08-02 08:22:22 +0800

    DEBUG: fixed error

--- routes/qna.js
+++ routes/qna.js
@@ -10,11 +10,20 @@ router.get('/', function(req, res, next) {
       throw err;
 
     var typeToName = [
+      "其他",
       "校園生活",
       "學生事務",
-      "宿舍生活",
-      "其他"
+      "宿舍生活"
     ];
+
+    var renderTime = function(date) {
+      var day = date.getDate();
+      var monthIndex = date.getMonth();
+      var year = date.getFullYear();
+      var time = year + '/' + (++monthIndex) + '/' + day;
+      return time;
+    }
+
     res.render('qna/index', {
       title: 'QnA',
       user: req.user,
@@ -27,39 +36,30 @@ router.get('/', function(req, res, next) {
 
 // 新增問題
 router.post('/', isLoggedIn, function(req, res) {
+
   var qna = new Qna();
   qna.userId = req.user._id;
-  qna.type = checkType(req.type);
+  qna.type = (function(reqBodyType) {
+    if (reqBodyType > 3 || reqBodyType < 0)
+      return 3;
+    else
+      return Math.round(reqBodyType);
+  })(req.body.type);
   qna.title = req.body.title;
   qna.content = req.body.content;
-  qna.save();
-  res.redirect('/qna');
+  qna.save(function(err) {
+    if (err)
+      return handleError(err);
+    else
+      res.redirect('/qna/?post=success');
+  });
 });
 
-function checkType(type) {
-
-  if (type > 3 || type < 0) {
-    return 3;
-  }
-
-  return round(type);
-}
-
 function isLoggedIn(req, res, next) {
-
-  if (!req.isAuthenticated()) {
+  if (!req.isAuthenticated())
     res.redirect('/login');
-  }
-
-  return next();
-}
-
-function renderTime(date) {
-  var day = date.getDate();
-  var monthIndex = date.getMonth();
-  var year = date.getFullYear();
-  var time = year + '/' + (++monthIndex) + '/' + day;
-  return time;
+  else
+    return next();
 }
 
 module.exports = router;
