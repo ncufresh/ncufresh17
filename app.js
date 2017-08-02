799acc677b63d289f028df9d6a59e1f49287431c
cytsai <evan11401@gmail.com>
2017-08-02 12:36:12 +0800

    error page

--- app.js
+++ app.js
@@ -122,7 +122,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',{
+    title: '頁面不存在 ｜ 新生知訊網',
+    user: req.user,
+  });
 });
 
 module.exports = app;
